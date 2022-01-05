74630d74d1a34160b4c353c6be2887fd595ce09d
BlackDynamite <rata625@outlook.com>
2022-01-04 22:29:53 -0600

    Modial web app
    Modual web app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from "./components/Todo";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>My Todo</h1>
+      <Todo text='List 1' />
+      <Todo text='List 2' />
+      <Todo text='List 3' />
+      <Todo text='List 4' />
+    </div >
   );
 }
 
